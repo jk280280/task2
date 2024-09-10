c6adce64be3ccb224e7118a940ab520ed8a21599
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-10 13:50:07 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,6 +1,20 @@
+
 output "cluster_name" {
-  value = aws_eks_cluster.eks.name
+  value = aws_eks_cluster.this.name
 }
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "node_group_name" {
+  value = aws_eks_node_group.this.node_group_name
+}
+
 
 output "kubeconfig" {
   value = aws_eks_cluster.eks.endpoint
