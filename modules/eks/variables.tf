ec2bab8119d6f11b80d8f65a96853ae98738b092
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-10 13:48:54 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,5 +1,22 @@
-variable "cluster_name" {}
-variable "node_group_name" {}
-variable "instance_types" {
-  type = list(string)
+variable "region" {
+  description = "The AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
 }
+
+variable "cluster_name" {
+  description = "The name of the EKS cluster"
+  type        = string
+}
+
+variable "node_group_name" {
+  description = "The name of the EKS node group"
+  type        = string
+}
+
+
+variable "subnet_ids" {
+  description = "The VPC subnets to deploy the EKS cluster into"
+  type        = list(string)
+}
+
