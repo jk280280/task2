091823542aeb617a697ae2f7d455f1330e04a0dc
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-10 13:38:08 +0530

    Update main.tf

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -22,7 +22,7 @@ resource "aws_eks_cluster" "eks" {
 }
 
 resource "aws_eks_node_group" "node" {
-  cluster_name    = aws_eks_cluster.this.name
+  cluster_name    = aws_eks_cluster.eks.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.eks.arn
   subnet_ids      = data.aws_subnets.default.ids
