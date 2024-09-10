52f2015532780ee6727c5f2bfee151f64a0db238
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-10 13:38:38 +0530

    Update outputs.tf

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster_name" {
-  value = aws_eks_cluster.this.name
+  value = aws_eks_cluster.eks.name
 }
 
 output "kubeconfig" {
-  value = aws_eks_cluster.this.endpoint
+  value = aws_eks_cluster.eks.endpoint
 }
 
 output "eks_role_arn" {
