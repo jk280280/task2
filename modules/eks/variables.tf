6772ba4d921803fb39bbaf043c843fdd35ed88b5
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-12 20:51:16 +0530

    Update variables.tf

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "us-west-1"
 }
 
 variable "cluster_name" {
